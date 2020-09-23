1c131960ce9e954e17e755a7fa672552ec8c7fa6
Amir Beyk <amir.mrb@gmail.com>
2020-09-23 15:14:06 +0330

    [*] ADDs mock activity for instrumented testing
    also remove some wronge tracked files by GIT

---
+++ SportBuff/app/src/main/java/com/buffup/app/MockActivity.kt
@@ -0,0 +1,16 @@
+package com.buffup.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (layout == 0) throw IllegalArgumentException("layout parameter should be valuated before launching activity")
+        setContentView(layout)
+    }
+
+    companion object {
+        var layout = 0
+    }
+}
