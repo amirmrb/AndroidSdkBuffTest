aac001529fef8072f6730ca24429540b0956d078
Amir Beyk <amir.mrb@gmail.com>
2020-09-23 15:16:45 +0330

    [+] ADDs basic gradle dependencies and question over video skeleton for TDD

--- SportBuff/buffsdk/src/main/java/com/buffup/buffsdk/IQuestionOverVideo.kt
+++ SportBuff/buffsdk/src/main/java/com/buffup/buffsdk/IQuestionOverVideo.kt
@@ -1,5 +1,18 @@
 package com.buffup.buffsdk
 
-interface IQuestionOverVideo {
-
+interface IQuestionOverVideo<T> {
+    var status: Status
+    fun init()
+    fun start()
+    fun getRemainingTime(): Long
+    fun submitAnswer(answer: T)
+    fun close()
 }
+
+
+sealed class Status
+
+object FetchData : Status()
+object Showing : Status()
+object SubmittedAnswerStatus : Status()
+object IdealStatus : Status()
